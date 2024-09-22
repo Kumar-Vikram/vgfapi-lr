a27f80d38c328f02dabb26da514662654dcb3f41
Kumar-Vikram <krvikram@yahoo.com>
2024-09-22 12:13:52 +0530

    Add files via upload

---
+++ model_app.py
@@ -0,0 +1,32 @@
+import warnings
+warnings.filterwarnings('ignore')
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+app = FastAPI()
+
+class Input(BaseModel):
+    CONSOLE: object
+    YEAR: int
+    CATEGORY: object
+    PUBLISHER: object
+    RATING: object
+    CRITICS_POINTS: float
+    USER_POINTS:float
+
+class Output(BaseModel):
+    SalesInMillions: float
+@app.post("/predict")
+def predict(data: Input) -> Output:
+    X_input = pd.DataFrame([[data.CONSOLE,data.YEAR,data.CATEGORY,data.PUBLISHER,data.RATING,data.CRITICS_POINTS,data.USER_POINTS]],columns=['CONSOLE', 'YEAR', 'CATEGORY', 'PUBLISHER',
+       'RATING', 'CRITICS_POINTS', 'USER_POINTS'])
+    # Load the model using pikle file
+    model = joblib.load('video_game_sales_prediction.pkl')
+
+    # predict using model
+    prediction = model.predict(X_input)
+
+    # return output
+    return Output(SalesInMillions = prediction)
